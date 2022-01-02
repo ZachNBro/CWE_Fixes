09db9d291ee55c26ee0ddad46093cb48457ff3a3
ZachNBro <96999736+ZachNBro@users.noreply.github.com>
2022-01-02 15:49:02 -0600

    Update MissingAuth.java

--- MissingAuth.java
+++ MissingAuth.java
@@ -1,10 +1,17 @@
+/**
+* File: MissingAuth.java
+* Author: Zachary N. Brown
+* Purpose: Demonstrates CWE-306: Missing Authentication 
+* for Critical Function that does not authenticate whether
+* the user posesses the access level or not.
+*/
+
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class MissingAuth {
-	
-	
+		
 	public static void main(String[] args) {
 
 		InputStreamReader isReader = new InputStreamReader(System.in);
@@ -13,7 +20,7 @@ public class MissingAuth {
 		try {
 			System.out.println("Enter Name:");
 			String name = (stdin.readLine());
-			System.out.println("Enter Access Level:");
+			System.out.println("Enter Access Level:"); // User can specify any access level
 			int access = Integer.parseInt(stdin.readLine());
 			System.out.println("Enter SSN:");
 			int ssn = Integer.parseInt(stdin.readLine());
