b677f659c068cb40ebe10de0c30451df17cf84c9
ZachNBro <96999736+ZachNBro@users.noreply.github.com>
2022-01-02 18:07:49 -0600

    Update ExcessiveAuth.java

--- ExcessiveAuth.java
+++ ExcessiveAuth.java
@@ -9,37 +9,33 @@
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.io.IOException;
+import java.io.*;
 
 public class ExcessiveAuth {
 
     public static void main(String[] args) throws IOException {
-
-        menu();
-        
-    }
-
-        public static void menu() throws IOException{
-            
-            String validation = "aQ5t&y!"; // passwords should never be hardcoded (used for simple demonstrative purposes only)
-					   // store passwords elsewhere with a hash and salt algorithm
-            
+	
+	// input file with pass to compare
+    	File file = new File("C:\\Users\\Zack\\eclipse-workspace\\ExcessiveAuth\\src\\pass.txt");
+    	BufferedReader br = new BufferedReader(new FileReader(file));
+    	String validation = br.readLine();
+ 
             InputStreamReader isReader = new InputStreamReader(System.in);
             BufferedReader stdin = new BufferedReader(isReader);
 
-			while (true) {
+		while (true) {
 			
-			System.out.println("What would you like to do?");
-			System.out.println("1. Login");
-			System.out.println("2. Exit");
+		System.out.println("What would you like to do?");
+		System.out.println("1. Login");
+		System.out.println("2. Exit");
 				
-			int input = Integer.parseInt(stdin.readLine());
+		int input = Integer.parseInt(stdin.readLine());
 	            
-	            if (input == 1) {
-	            System.out.println("Enter your username:");
-	            String username = (stdin.readLine());
-	            System.out.println("Enter your password:");
-	            String password = (stdin.readLine());
+	        if (input == 1) {
+	        System.out.println("Enter your username:");
+	        String username = (stdin.readLine());
+	        System.out.println("Enter your password:");
+	        String password = (stdin.readLine());
                     
                     if(password.equals(validation)) {
 	                    System.out.println("Welcome back!");
@@ -56,9 +52,9 @@ public class ExcessiveAuth {
 	            	
 	            } else {
 	                System.out.println("Invalid input");
-	                menu();
+	                
 	            } 
+		}
 	           
-            } 
-        } 
-}
+       } 
+ } 
