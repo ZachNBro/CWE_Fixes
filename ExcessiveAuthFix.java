e1e99f5f910a3379acb5e26e580a711a9a83a193
ZachNBro <96999736+ZachNBro@users.noreply.github.com>
2022-01-02 18:14:16 -0600

    Update ExcessiveAuthFix.java

--- ExcessiveAuthFix.java
+++ ExcessiveAuthFix.java
@@ -1,38 +1,24 @@
-/**
-* File: ExcessiveAuthFix.java
-* Author: Zachary N. Brown
-* Purpose: Demonstrates a mitigation for CWE-307: 
-* Improper Restriction of Excessive Authentication 
-* Attempts by limiting the number of attempts
-* and exiting the application if authentication
-* cannot be reached.
-*/
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.io.IOException;
+import java.io.*;
 
 public class ExcessiveAuthFix {
 
     public static void main(String[] args) throws IOException {
+	
+    	File file = new File("C:\\Users\\Zack\\eclipse-workspace\\ExcessiveAuth\\src\\pass.txt");
+    	BufferedReader br = new BufferedReader(new FileReader(file));
+    	String validation = br.readLine();
+ 
+        InputStreamReader isReader = new InputStreamReader(System.in);
+        BufferedReader stdin = new BufferedReader(isReader);
+	    
+        // include starting value for counter
+        int login_attempts = 1;
 
-        menu();
-        
-    }
-
-        public static void menu() throws IOException{
-            
-            String validation = "aQ5t&y!"; // passwords should never be hardcoded (used for simple demonstrative purposes)
-					   // store passwords elsewhere with a hash and salt algorithm
-            // include starting value for counter
-            int login_attempts = 1;
-            
-            InputStreamReader isReader = new InputStreamReader(System.in);
-            BufferedReader stdin = new BufferedReader(isReader);
-
-			while (true) {
+		while (true) {
 			
-			// condition for max login attempts
+		// condition for max login attempts
 			if (login_attempts <= 3) {
 			
 			System.out.println("What would you like to do?");
@@ -41,21 +27,21 @@ public class ExcessiveAuthFix {
 				
 			int input = Integer.parseInt(stdin.readLine());
 	            
-	            if (input == 1) {
-	            System.out.println("Enter your username:");
-	            String username = (stdin.readLine());
-	            System.out.println("Enter your password:");
-	            String password = (stdin.readLine());
+	           if (input == 1) {
+	        	   System.out.println("Enter your username:");
+	        	   String username = (stdin.readLine());
+	        	   System.out.println("Enter your password:");
+	        	   String password = (stdin.readLine());
                     
-                    if(password.equals(validation)) {
-	                    System.out.println("Welcome back!");
-	                    break;
+                   if(password.equals(validation)) {
+                	   System.out.println("Welcome back!");
+                	   break;
 
                     } else {
-	                    System.out.println("Invalid");
-	                    // increment counter
-	                    login_attempts++;
-	                    continue;
+	                   System.out.println("Invalid");
+	                   // increment counter
+	                   login_attempts++;
+	                   continue;
                     }
                    
 	            } else if (input == 2) {
@@ -64,7 +50,6 @@ public class ExcessiveAuthFix {
 	            	
 	            } else {
 	                System.out.println("Invalid input");
-	                menu();
 	            }
 	           
             } else {
