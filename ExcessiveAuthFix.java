fc02d9cc3804abdb49e3ae1e6873636d026183e3
ZachNBro <96999736+ZachNBro@users.noreply.github.com>
2022-01-02 18:35:00 -0600

    Update ExcessiveAuthFix.java

--- ExcessiveAuthFix.java
+++ ExcessiveAuthFix.java
@@ -7,8 +7,6 @@
 * and stop a user from conducting a brute force attack.
 */
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.*;
 
 public class ExcessiveAuthFix {
