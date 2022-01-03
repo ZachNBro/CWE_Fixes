64acba87b1e19a667000c5c6c2449a7ab35fe39f
ZachNBro <96999736+ZachNBro@users.noreply.github.com>
2022-01-02 18:32:42 -0600

    Update MissingAuthFix.java

--- MissingAuthFix.java
+++ MissingAuthFix.java
@@ -7,27 +7,25 @@
 * 
 */
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
+import java.io.*;
 
-public class MissingAuthExample {
+public class MissingAuthFix {
 		
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 	
 	File file = new File("path to validcode.txt");
     	BufferedReader br = new BufferedReader(new FileReader(file));
     	String validation = br.readLine();
 	
-        InputStreamReader isReader = new InputStreamReader(System.in);
+    	InputStreamReader isReader = new InputStreamReader(System.in);
 	BufferedReader stdin = new BufferedReader(isReader);
 		
 		try {	
 			
 		System.out.println("Enter Employee Code:");
-		int ecode = Integer.parseInt(stdin.readLine());
+		String ecode = (stdin.readLine());
 			
-			if (ecode == validation) {
+			if (ecode.equals(validation)) {
 			
 			System.out.println("Enter Name:");
 			String name = (stdin.readLine());
